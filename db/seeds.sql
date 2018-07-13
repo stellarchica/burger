2302de9b11f3e5aca0887cb717b7709227429e96
sarah martinez <sarahmartinez@sarahs-macbook.attlocal.net>
2018-07-13 18:49:02 -0500

    minor seed change

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,21 +3,4 @@ INSERT INTO burgers (burger_name) VALUES ("Breakfast Burger");
 INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
 INSERT INTO burgers (burger_name) VALUES ("Mushroom Swiss Burger");
 
-INSERT INTO menu (item) VALUES ("Veggie Burger");
-INSERT INTO menu (item) VALUES ("Breakfast Burger");
-INSERT INTO menu (item) VALUES ("Bacon Burger");
-INSERT INTO menu (item) VALUES ("Mushroom Swiss Burger");
-INSERT INTO menu (item) VALUES ("BBQ Burger");
-INSERT INTO menu (item) VALUES ("Sriracha Burger");
-INSERT INTO menu (item) VALUES ("Avocado Burger");
-INSERT INTO menu (item) VALUES ("Double Meat Burger");
-INSERT INTO menu (item) VALUES ("Cheddar Burger");
-INSERT INTO menu (item) VALUES ("Plain Burger");
-
-INSERT INTO menu (item, entree) VALUES ("French Fries", 0);
-INSERT INTO menu (item, entree) VALUES ("Curly Fries", 0);
-INSERT INTO menu (item, entree) VALUES ("Waffle Fries", 0);
-INSERT INTO menu (item, entree) VALUES ("Loaded Fries", 0);
-INSERT INTO menu (item, entree) VALUES ("Onion Rings", 0);
-INSERT INTO menu (item, entree) VALUES ("Tater Tots", 0);
-INSERT INTO menu (item, entree) VALUES ("Fried Veggies", 0);
+SELECT * FROM burgers;
